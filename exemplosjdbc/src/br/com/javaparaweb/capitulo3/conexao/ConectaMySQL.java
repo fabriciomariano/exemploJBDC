7925347c5add3aa599a8909bc955d1b85d01963a
Fabricio Mariano <fabriciomariano@gmail.com>
2021-12-04 16:43:29 -0300

    First commit

---
+++ exemplosjdbc/src/br/com/javaparaweb/capitulo3/conexao/ConectaMySQL.java
@@ -0,0 +1,24 @@
+package br.com.javaparaweb.capitulo3.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectaMySQL {
+	
+	public static void main(String[] args) {
+		Connection conexao = null;
+		
+		try {
+			String url = "jdbc:mysql://localhost/agenda";
+			String usuario = "root";
+			String senha = "230102se";
+			conexao = DriverManager.getConnection(url, usuario, senha);
+			System.out.println("Conectou!");
+			conexao.close();
+		} catch (SQLException e) {
+			System.out.println("Ocorreu um erro ao criar a conexão com o banco de dados"+ e.getMessage());
+		}
+	}
+
+}
